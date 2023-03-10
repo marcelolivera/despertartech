3401890b8eb876700da2deaaa6a0c2f485238004
marcelolivera <126104779+marcelolivera@users.noreply.github.com>
2023-03-10 15:12:13 -0300

    Add files via upload

---
+++ oficina03.js
@@ -0,0 +1,16 @@
+ let temDinheiro = true;
+ let temSol = false;
+
+ if (temDinheiro) {
+        if (temSol) {
+    console.log("Guarujá");
+    } else {
+     console.log("Restaurante");
+    }
+} else {
+    if (temSol) {
+        console.log("Ibirapuera");
+    } else {
+        console.log("Aula");
+    }
+}
